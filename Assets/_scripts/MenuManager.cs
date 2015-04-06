452dce4c88808f39ba21566ea31fb4655305af04
jLett <jack.maillett@gmail.com>
2015-04-06 15:21:07 -0400

    Added Photon and basic menu switching

--- Assets/_scripts/MenuManager.cs
+++ Assets/_scripts/MenuManager.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject mainMenu, optionsMenu, playMenu;
+
+	public string roomName;
+	private RoomInfo[] roomsList;
+
 	void Start () {
-	
+		PhotonNetwork.ConnectUsingSettings("0.0.1");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update() {
+
+	}
+
+	void OnReceivedRoomListUpdate() {
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+
+	void OnJoinedRoom() {
+		Debug.Log("connected to room");
+	}
+
+	//--------------------------------------------------button functions--------------------------------------------------
+
+	//main menu buttons
+	public void ShowPlayMenu() {
+		mainMenu.SetActive(false);
+		optionsMenu.SetActive(false);
+		playMenu.SetActive(true);
+	}
+
+	public void ShowOptionsMenu() {
+		mainMenu.SetActive(false);
+		optionsMenu.SetActive(true);
+		playMenu.SetActive(false);
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+
+	public void ShowMainMenu() {
+		mainMenu.SetActive(true);
+		optionsMenu.SetActive(false);
+		playMenu.SetActive(false);
 	}
 }
