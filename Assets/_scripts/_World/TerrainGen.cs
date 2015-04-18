f948d1fd170d25dbdd71210e1a1b1b1d7ee3ed40
jLett <jack.maillett@gmail.com>
2015-04-18 18:58:32 -0400

    caves!

--- Assets/_scripts/_World/TerrainGen.cs
+++ Assets/_scripts/_World/TerrainGen.cs
@@ -7,7 +7,7 @@ public class TerrainGen {
 	//base layer
 	float stoneBaseHeight = -24;
 	float stoneBaseNoise = 0.05f;//noise frequency of layer
-	float stoneBaseNoiseHeight = 4;//amplitude of noise
+	float stoneBaseNoiseHeight = 4;//max amplitude of noise
 
 	//mountain/hill layer
 	float stoneMountainHeight = 48;
@@ -15,9 +15,13 @@ public class TerrainGen {
 	float stoneMinHeight = -12;
 
 	//dirt layer
-	float dirtBaseHeight = 2;
+	float dirtBaseHeight = 4;
 	float dirtNoise = 0.04f;
 	float dirtNoiseHeight = 2;
+
+	//caves
+	float caveFrequency = 0.02f;
+	int caveSize = 20;
 
 	public Chunk ChunkGen(Chunk chunk) {
 		for(int x = chunk.pos.x; x < chunk.pos.x + Chunk.chunkSize; x++) {//using world coords
@@ -30,33 +34,40 @@ public class TerrainGen {
 
 	public Chunk ChunkColumnGen(Chunk chunk, int x, int z) {
 		int stoneHeight = Mathf.FloorToInt(stoneBaseHeight);																			//set base height
-		stoneHeight += GetNoise(x, 0, z, chunk.world.seed, stoneMountainFrequency, Mathf.FloorToInt(stoneMountainHeight), 0.6f);		//add mountain noise
+		stoneHeight += GetPinkNoise(x, 0, z, chunk.world.seed, stoneMountainFrequency, Mathf.FloorToInt(stoneMountainHeight), 0.6f);		//add mountain noise
 		if(stoneHeight < stoneMinHeight)
 			stoneHeight = Mathf.FloorToInt(stoneMinHeight);																				//raise all stone up to min height (if needed)
-		stoneHeight += GetNoise(x, 0, z, chunk.world.seed, stoneBaseNoise, Mathf.FloorToInt(stoneBaseNoiseHeight), 0.2f);				//add base noise
+		stoneHeight += GetPinkNoise(x, 0, z, chunk.world.seed, stoneBaseNoise, Mathf.FloorToInt(stoneBaseNoiseHeight), 0.2f);				//add base noise
 
 		int dirtHeight = stoneHeight + Mathf.FloorToInt(dirtBaseHeight);																//set base dirt height
-		dirtHeight += GetNoise(x, 100, z, chunk.world.seed, dirtNoise, Mathf.FloorToInt(dirtNoiseHeight), 0.2f);						//add noise to dirt height
+		dirtHeight += GetPinkNoise(x, 100, z, chunk.world.seed, dirtNoise, Mathf.FloorToInt(dirtNoiseHeight), 0.2f);						//add noise to dirt height																							//add layer of grass on top
 
-		int grassHeight = dirtHeight + 1;																								//add layer of grass on top
+		for(int y = chunk.pos.y; y < chunk.pos.y + Chunk.chunkSize; y++) {															//decide which block goes where based on height
+			int caveChance = GetRidgeNoise(x, y, z, chunk.world.seed, caveFrequency, 100);
 
-		for(int y = chunk.pos.y; y < chunk.pos.y + Chunk.chunkSize; y++) {																//decide which block goes where based on height
-			if(y <= stoneHeight) {
+			if(y <= stoneHeight && caveSize < caveChance) {
 				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockStone());
-			} else if(y <= dirtHeight) {
+			} else if(y <= dirtHeight && caveSize < caveChance) {
 				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockDirt());
-			} else if(y <= grassHeight) {
+			} else if(y <= dirtHeight + 1 && caveSize < caveChance) {
 				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockGrass());
 			} else {
 				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockAir());
 			}
 		}
+
 		return chunk;
 	}
 
-	public static int GetNoise(int x, int y, int z, string seed, float scale, int max, float persistence) {
+	public static int GetPinkNoise(int x, int y, int z, string seed, float scale, int max, float persistence) {
 		PinkNoise n = new PinkNoise(seed.GetHashCode());
 		n.Persistence = persistence;
 		return Mathf.FloorToInt((n.GetValue(x*scale, y*scale, z*scale) + 1f) * (max/2f));
+	}
+
+	public static int GetRidgeNoise(int x, int y, int z, string seed, float scale, int max) {
+		RidgeNoise n = new RidgeNoise(seed.GetHashCode());
+		n.Exponent = 0.8f;
+		return Mathf.FloorToInt(((1f - n.GetValue(x*scale, y*scale, z*scale)) + 1f) * (max/2f));
 	}
 }
